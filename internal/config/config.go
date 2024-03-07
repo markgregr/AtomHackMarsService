c6906ce7db6303740e666ce5cfac1cab09ff25d3
Mark Grevtsov <markgrevtsov2310@gmail.com>
2024-03-08 00:48:24 +0300

    config .env update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"os"
+
 	"github.com/caarlos0/env/v6"
 	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
@@ -8,12 +10,14 @@ import (
 
 func NewConfig() (*App, error) {
 	var cfg App
-	err := godotenv.Load()
-    if err != nil {
-        log.Fatalf("unable to load .env file: %e", err)
-    }
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("unable to load .env file: %v", err)
+		}
+	}
+
 	if err := env.Parse(&cfg); err != nil {
-		panic(err)
+		log.Fatalf("error parsing environment variables: %v", err)
 	}
 	log.Println("cfg: ", cfg)
 	level, err := log.ParseLevel(cfg.ErrorLevel)
